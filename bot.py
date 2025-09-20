549bbf695671e102a2627f99716c046dfdf25f3f
NeuroLuxBot <alekseyvorona0708@gmail.com>
2025-09-20 11:05:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
-logger = logging.getLogger(name)
+logger = logging.getLogger(__name__)
 # === /start ===
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
